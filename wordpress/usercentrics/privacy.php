d82e3acaac3232a570f80bbf5f63e4fe6574315f
khanhvo <vodk23@#>
2025-09-15 15:52:54 +0700

    Add UserCentrics code to the header

---
+++ wordpress/usercentrics/privacy.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Privacy Plugin
+ * Plugin URI: https://sixfive.io
+ * Description: MU plugin
+ * Version: 1.0.1
+ * Author: SixFive
+ * Author URI: https://sixfive.io
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+add_action( 'wp_head', 'sf_wp_head_uc', 9 );
+function sf_wp_head_uc() {
+	echo '<link rel="preconnect" href="//privacy-proxy.usercentrics.eu"> <link rel="preload" href="//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js" as="script"> 
+<script type="application/javascript" src="https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js"></script>
+<script>uc.setCustomTranslations("https://termageddon.ams3.cdn.digitaloceanspaces.com/translations/");</script>
+<!-- Ignore Blocking for GTM -->
+<script>
+ uc.deactivateBlocking([
+	 "BJ59EidsWQ", // GTM is not blocked
+ ]);
+</script>';
+}
