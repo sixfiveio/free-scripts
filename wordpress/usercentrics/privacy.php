3db2c9b00434394d2f0a8fadd50debb02415f8b7
khanhvo <vodk23@#>
2025-09-23 15:20:05 +0700

    Add UserCentrics code to the header

--- wordpress/usercentrics/privacy.php
+++ wordpress/usercentrics/privacy.php
@@ -12,8 +12,9 @@
 defined( 'ABSPATH' ) || exit;
 
 add_action( 'wp_head', 'sf_wp_head_uc', 9 );
-function sf_wp_head_uc() {
-	echo '<link rel="preconnect" href="//privacy-proxy.usercentrics.eu"> <link rel="preload" href="//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js" as="script"> 
+if ( ! function_exists( 'sf_wp_head_uc' ) ) {
+	function sf_wp_head_uc() {
+		echo '<link rel="preconnect" href="//privacy-proxy.usercentrics.eu"> <link rel="preload" href="//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js" as="script"> 
 <script type="application/javascript" src="https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js"></script>
 <script>uc.setCustomTranslations("https://termageddon.ams3.cdn.digitaloceanspaces.com/translations/");</script>
 <!-- Ignore Blocking for GTM -->
@@ -22,4 +23,5 @@ function sf_wp_head_uc() {
 	 "BJ59EidsWQ", // GTM is not blocked
  ]);
 </script>';
+	}
 }
