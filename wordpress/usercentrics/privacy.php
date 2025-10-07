69b84d8f19ad3a13c344497c22c6cc6f0debfd28
khanhvo <vodk23@#>
2025-10-07 15:20:00 +0700

    Add Consent status to DFP Ads

--- wordpress/usercentrics/privacy.php
+++ wordpress/usercentrics/privacy.php
@@ -11,9 +11,9 @@
 
 defined( 'ABSPATH' ) || exit;
 
-add_action( 'wp_head', 'sf__wp_head_uc', 9 );
-if ( ! function_exists( 'sf__wp_head_uc' ) ) {
-	function sf__wp_head_uc() {
+add_action( 'wp_head', 'sf_mu_wp_head_uc', 9 );
+if ( ! function_exists( 'sf_mu_wp_head_uc' ) ) {
+	function sf_mu_wp_head_uc() {
 		echo '<link rel="preconnect" href="//privacy-proxy.usercentrics.eu"> <link rel="preload" href="//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js" as="script"> 
 <script type="application/javascript" src="https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js"></script>
 <script>uc.setCustomTranslations("https://termageddon.ams3.cdn.digitaloceanspaces.com/translations/");</script>
@@ -23,5 +23,20 @@ if ( ! function_exists( 'sf__wp_head_uc' ) ) {
 	 "BJ59EidsWQ", // GTM is not blocked
  ]);
 </script>';
+	}
+}
+
+// Consent status - DFP Ads.
+add_filter( 'script_loader_tag', 'sf_mu_script_loader_tag', 10, 3 );
+if ( ! function_exists( 'sf_mu_script_loader_tag' ) ) {
+	function sf_mu_script_loader_tag( $tag, $handle, $src ) {
+		if ( 'jquery_dfp' === $handle ) {
+			$tag = '<script async type="text/plain" src="' . esc_url( $src ) . '" id="' . $handle . '-js" data-usercentrics="DoubleClick Ad"></script>' . "\n";
+		}
+		if ( 'google_dfp' === $handle ) {
+			$tag = '<script async type="text/plain" src="' . esc_url( $src ) . '" id="' . $handle . '-js" data-usercentrics="DoubleClick Ad"></script>' . "\n";
+		}
+
+		return $tag;
 	}
 }
